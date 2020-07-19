8531f225e1cf29ad0a459619348487eba8b01049
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-07-19 23:55:52 +0200

    Update index.js
    request, response GET

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express'); //Importar librerias de node
 
 const app = express(); //la app
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Meower! 😹 🐈'
+    });
+});
+
 app.listen(5000, () => {
     console.log('Listening on http://localhost:5000');
-  });
+});
