f9141ebd86f51426cacd42999c54066ce68c3e2c
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-07-20 08:06:10 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,17 +7,25 @@ const app = express(); //la app
 app.use(cors());
 app.use(express.json());
 
+//Cuando se intenta acceder a http://localhost:5000
 app.get('/', (req, res) => {
     res.json({
         message: 'Meownjnner! 😹 🐈'
     });
 });
 
+//Asegurarnos que es contenido valido -> no es vacio, ni el titulo ni el contenido
+function isValidMew(mew) {
+    return mew.name && mew.name.toString().trim() !== '' && mew.name.toString().trim().length <= 50 &&
+    mew.content && mew.content.toString().trim() !== '' && mew.content.toString().trim().length <= 140;
+}
+
 //Aqui entra cuando se hace un nuevo mew (nuevo mensaje)
 app.post('/mews', (req, res) => {
     if(isValidMew(req, res)){
         //Si es valido lo añadimos a la BD
         console.log("Inserting into DB...");
+        
     }
     else{
         res.status(422);
