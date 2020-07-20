8cb11c96cd0f29ddd0472c5d8ff63c0c0616e8c2
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-07-20 07:49:20 +0200

    CORS ACCES CONTROL ALLOW ORIGIN
    CORS ACCES CONTROL ALLOW ORIGIN

--- client/client.js
+++ client/client.js
@@ -2,11 +2,13 @@ console.log('Hello World!');
 
 const form = document.querySelector('form'); // grabbing an element on the page
 const loadingElement = document.querySelector('.loading');
+const API_URL = 'http://localhost:5000/mews'; //where is the server that im making the request
 
 loadingElement.style.display = 'none';
 
 form.addEventListener('submit', (event) => {
   event.preventDefault(); //Poner para hacer que trabajemos con JS
+  //Recoger los datos del form de html
   const formData = new FormData(form);
   const name = formData.get('name');
   const content = formData.get('content');
@@ -16,7 +18,16 @@ form.addEventListener('submit', (event) => {
     content
   };
 
-  console.log(mew)
+  //Ocultar form y mostrar el loading element -> el gif
   form.style.display = 'none';
   loadingElement.style.display = '';
+
+  //Enviar a backend API en forma de JSON
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(mew),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
 });
