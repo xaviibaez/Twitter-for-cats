fbf66cb264b1c3e66e0e1b0884269e4adac17dfe
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-07-25 18:51:56 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ mongo -> una vez instalado para gestionar mongo des de la consola
 //Cuando se intenta acceder a http://localhost:5000
 app.get('/', (req, res) => {
     res.json({
-        message: 'Meowggdddggnjnner! 😹 🐈'
+        message: 'Meowggddcdsvsdvsdvsdvsdvdggnjnner! 😹 🐈'
     });
 });
 
