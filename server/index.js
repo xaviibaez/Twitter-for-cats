84dffa63bb757df026f5fdb4e45af657c71a0bf5
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-07-26 13:56:18 +0200

    Update index.js
    auto tab

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 //Importar librerias de node
-const express = require('express'); 
+const express = require('express');
 //Cors
 const cors = require('cors');
 //Conexion a MongoDB
@@ -10,7 +10,7 @@ const Filter = require('bad-words');
 const rateLimit = require('express-rate-limit');
 
 //Objeto App
-const app = express(); 
+const app = express();
 
 //Conectar a dominio/nombreBBDD
 const db = monk('localhost/meower');
@@ -60,27 +60,27 @@ app.get('/', (req, res) => {
 //Cuando se intenta acceder a http://localhost:5000/mews se deberian listar todos las pruebas realizadas.
 app.get('/mews', (req, res) => {
     mews
-      .find()
-      .then(mews => {
-        res.json(mews);
-      });
-  });
+        .find()
+        .then(mews => {
+            res.json(mews);
+        });
+});
 
 //Asegurarnos que es contenido valido -> no es vacio, ni el titulo ni el contenido
 function isValidMew(mew) {
     return mew.name && mew.name.toString().trim() !== '' &&
-    mew.content && mew.content.toString().trim() !== '';
+        mew.content && mew.content.toString().trim() !== '';
 }
 
 //Poner el rate limit despues de hacer la peticion
 app.use(rateLimit({
     windowMs: 30 * 1000, // 30 seconds
     max: 1
-  }));
+}));
 
 //Aqui entra cuando se hace un nuevo mew (nuevo mensaje)
 app.post('/mews', (req, res) => {
-    if(isValidMew(req.body)){
+    if (isValidMew(req.body)) {
         //Si es valido lo añadimos a la BD
         console.log("Inserting into DB...");
 
@@ -89,18 +89,18 @@ app.post('/mews', (req, res) => {
             name: filter.clean(req.body.name.toString()),
             content: filter.clean(req.body.content.toString()),
             created: new Date()
-          };
+        };
 
         console.log(mew);
 
         //Hacemos insert en la BBDD
         mews
-        .insert(mew)
-        .then(createdMew => {
-            res.json(createdMew);
-        });
+            .insert(mew)
+            .then(createdMew => {
+                res.json(createdMew);
+            });
     }
-    else{
+    else {
         //Si se pone contenido string vacio salta este error, no se muestra en pantalla
         res.status(422);
         res.json({
